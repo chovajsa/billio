b506500303eccd05e6c4c7bdacc782562f45cd02
System Administrator <root@Miroslavs-MacBook-Air.local>
2014-11-07 16:45:16 +0100

    ipt mail

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
+    'supportEmail' => 'ipt@trila.sk',
     'user.passwordResetTokenExpire' => 3600,
 
     'roles'=>[
